e55a54133fc153772237d73da13084e47128a6b4
Francis Gregori Munis <contato@francisgregori.com.br>
2026-04-26 20:21:56 -0300

    feat(host): rotating file logger using lumberjack

--- native-host/go.mod
+++ native-host/go.mod
@@ -1,3 +1,5 @@
 module github.com/FrancisGregori/audio-tab-finder/native-host
 
 go 1.25.0
+
+require gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
---
+++ native-host/internal/logging/logger.go
@@ -0,0 +1,90 @@
+package logging
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"time"
+
+	"gopkg.in/natefinch/lumberjack.v2"
+)
+
+type Level int
+
+const (
+	LevelDebug Level = iota
+	LevelInfo
+	LevelWarn
+	LevelError
+	LevelFatal
+)
+
+func (l Level) String() string {
+	switch l {
+	case LevelDebug:
+		return "DEBUG"
+	case LevelInfo:
+		return "INFO"
+	case LevelWarn:
+		return "WARN"
+	case LevelError:
+		return "ERROR"
+	case LevelFatal:
+		return "FATAL"
+	}
+	return "UNKNOWN"
+}
+
+type Logger struct {
+	out   io.Writer
+	level Level
+	inner *log.Logger
+}
+
+func New(logDir string) (*Logger, error) {
+	if err := os.MkdirAll(logDir, 0755); err != nil {
+		return nil, fmt.Errorf("create log dir: %w", err)
+	}
+	rot := &lumberjack.Logger{
+		Filename:   filepath.Join(logDir, "host.log"),
+		MaxSize:    1, // 1 MB
+		MaxBackups: 3,
+		MaxAge:     30, // days
+		Compress:   false,
+	}
+	return &Logger{
+		out:   rot,
+		level: LevelInfo,
+		inner: log.New(rot, "", 0),
+	}, nil
+}
+
+func (l *Logger) SetLevel(lvl Level) {
+	l.level = lvl
+}
+
+func (l *Logger) log(lvl Level, args ...any) {
+	if lvl < l.level {
+		return
+	}
+	prefix := fmt.Sprintf("%s [%s] ", time.Now().Format(time.RFC3339), lvl)
+	l.inner.Println(prefix + fmt.Sprint(args...))
+}
+
+func (l *Logger) Debug(args ...any) { l.log(LevelDebug, args...) }
+func (l *Logger) Info(args ...any)  { l.log(LevelInfo, args...) }
+func (l *Logger) Warn(args ...any)  { l.log(LevelWarn, args...) }
+func (l *Logger) Error(args ...any) { l.log(LevelError, args...) }
+func (l *Logger) Fatal(args ...any) {
+	l.log(LevelFatal, args...)
+	os.Exit(1)
+}
+
+func (l *Logger) Close() error {
+	if c, ok := l.out.(io.Closer); ok {
+		return c.Close()
+	}
+	return nil
+}
